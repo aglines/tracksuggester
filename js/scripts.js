23f2f3b8cb572f2928a0e85610b2cf890b401337
Andrew Glines <aglines@gmail.com>
2017-04-28 08:37:19 -0700

    Add HTML and JS code that implements a successfully working form

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("#survey").submit(function(event) {
+
+
+    var question1Input = $("#question1").val();
+    var question2Input = $("#question2").val();
+    var question3Input = $("#question3").val();
+    var question4Input = $("#question4").val();
+    var question5Input = $("#question5").val();
+
+    $(".question1").text(question1Input);
+    $(".question2").text(question2Input);
+    $(".question3").text(question3Input);
+    $(".question4").text(question4Input);
+    $(".question5").text(question5Input);
+
+
+    var suggestion = question1Input + " and " + question5Input;
+
+    $("#output").text(suggestion);
+
+    event.preventDefault();
+  });
+});
